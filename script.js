23e665af245baea89efa193e69e0ba55c2bae10a
PatrickFavret <110774860+PatrickFavret@users.noreply.github.com>
2025-06-03 18:59:28 -0400

    add rowCheck

--- script.js
+++ script.js
@@ -14,24 +14,25 @@ function Gameboard() {
     getBoard = () => board;
 
 
-    pickCell = (row, col, player) => {
-
-        row = prompt("give row: ");
-        col = prompt("give col: ");
+    pickCell = (row, col, playerToken) => {
         
         if (board[row][col].getValue() === 0){
-            board[row][col].addToken(player.token);
+            board[row][col].addToken(playerToken);
         }
         else
             console.log("Cell taken");
     }
 
     printBoard = () => {
+        console.log("Printing new board: ");
+        let rowTxt = ""
         for (let i = 0; i < rows; i++) {
             for (let j = 0; j < cols; j++) {
-                console.log(board[i][j]);
+                rowTxt += board[i][j].getValue();
             }
-            console.log('\n');
+            console.log(rowTxt);
+            console.log("\n");
+            rowTxt = ""
         }
     }
 
@@ -46,9 +47,9 @@ function Cell() {
         value = player
     };
 
-    const getValue = () => { 
-        value 
-    }
+    const getValue = () => {
+        return value
+    };
 
     return {
         addToken, getValue
@@ -56,30 +57,30 @@ function Cell() {
 }
 
 function GameController(playerOne, playerTwo) {
-    board = Gameboard();
+    const board = Gameboard();
     players = [
-        {   playerName: playerOne,
+        {   name: playerOne,
             token: 1
         },
 
         {
-            playerName: playerTwo,
+            name: playerTwo,
             token: 2
         }
     ]
 
-    activePlayer = players[0].playerName;
+    activePlayer = players[0];
 
     getActivePlayer = () => {
         activePlayer;
     }
 
     switchPlayerTurn = () => {
-        if (activePlayer === playerOne) {
-            activePlayer = playerTwo;
+        if (activePlayer === players[0]) {
+            activePlayer = players[1];
         }
         else {
-            activePlayer = playerOne
+            activePlayer = players[0]
         }
     }
 
@@ -87,8 +88,65 @@ function GameController(playerOne, playerTwo) {
         board.printBoard();
     }
 
-    playRound = () => {
+    playRound = (row, col) => {
         board.pickCell(row, col, activePlayer.token);
+    }
+
+
+    // check if any of the same token is in a row, column, or diagonal
+
+    let winner;
+    let arrBoard = board.getBoard();
+
+    rowCheck = () => {
+        for (let i = 0; i < rows; i++) {
+            let token = arrBoard[i][0].getValue();
+            for (let j = 0; j < cols; j++) {
+                if (arrBoard[i][j].getValue() != token) {
+                    console.log("no winner yet");
+                    continue;
+                } 
+                if (j == 2) {
+                    // console.log("row of same token");
+                    if (token == players[0].token) {
+                        winner = players[0];
+                        console.log(`The winner is ${winner.name}!`);
+                        return winner;
+                    } 
+                    else if (token == players[1].token) {
+                        winner = players[1];
+                        console.log(`The winner is ${winner.name}!`);
+                        return winner;
+                    }
+                }
+            }
+        }
+    }
+
+    colCheck = () => {
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                let token = arrBoard[0][j].getValue();
+                if (arrBoard[i][j].getValue() != token) {
+                    console.log("no winner yet");
+                    continue;
+                }
+                // potential bug right here, if on the last column the token matches then it might think the whole row matches 
+                if (j == 2) {
+                    // console.log("row of same token");
+                    if (token == players[0].token) {
+                        winner = players[0];
+                        console.log(`The winner is ${winner.name}!`);
+                        return winner;
+                    } 
+                    else if (token == players[1].token) {
+                        winner = players[1];
+                        console.log(`The winner is ${winner.name}!`);
+                        return winner;
+                    }
+                }
+            }
+        }
     }
 
     winCheck = () => {
@@ -102,19 +160,42 @@ function GameController(playerOne, playerTwo) {
 
     }
     
-    while (!winCheck()) {
-        printNewRound();
-        playRound();
-        switchPlayerTurn();
-    }
+    // while (!winCheck()) {
+    //     printNewRound();
+    //     playRound();
+    //     switchPlayerTurn();
+    // }
+
+    printNewRound();
+    playRound(2,0);
+    switchPlayerTurn();
+    
+    printNewRound();
+    playRound(1,0);
+    switchPlayerTurn();
+
+    printNewRound();
+    playRound(2,1);
+    switchPlayerTurn();
+
+    printNewRound();
+    playRound(1,1);
+    switchPlayerTurn();
+
+    printNewRound();
+    playRound(2,2);
+    switchPlayerTurn();
+
+    printNewRound();
+    playRound(1,1);
+    switchPlayerTurn();
+
+    rowCheck();
+    
 
     return { playRound, getActivePlayer, winCheck }
 }
 
 game = GameController("player 1", "player 2");
 
-while (!game.winCheck()) {
-    game.printNewRound();
-    game.playRound();
-    game.switchPlayerTurn();
-}
+
